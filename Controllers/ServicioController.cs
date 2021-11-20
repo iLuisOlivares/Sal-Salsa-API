3c6c4b28a113b6965baca508d6c91c6ff4361a92
Luis olivares <luissolivaresp@gmail.com>
2021-11-19 20:05:48 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/ServicioController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Restaurante_sal_salsa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicioController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        private IWebHostEnvironment _env;
+
+        public ServicioController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+        //consulta de servicios
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                        select id,nombre,descripcion,imagen
+                        from 
+                        servicio
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ Models/Servicios.cs
@@ -0,0 +1,11 @@
+﻿namespace Restaurante_sal_salsa.Models
+{
+    public class Servicios
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+
+        public string image { get; set; }
+    }
+}
