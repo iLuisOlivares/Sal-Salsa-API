2de33b37760518b4e9c543a864e04acd654bb5fc
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-11-23 13:38:26 -0500

    Improve Model and controller Methods of Servicio

--- Controllers/ServicioController.cs
+++ Controllers/ServicioController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using System.Data;
+using Restaurante_sal_salsa.Models;
 
 namespace Restaurante_sal_salsa.Controllers
 {
@@ -25,8 +26,8 @@ namespace Restaurante_sal_salsa.Controllers
         public JsonResult Get()
         {
             string query = @"
-                        select id,nombre,descripcion,imagen
-                        from 
+                        SELECT *
+                        FROM 
                         servicio
             ";
 
@@ -48,5 +49,130 @@ namespace Restaurante_sal_salsa.Controllers
 
             return new JsonResult(table);
         }
+
+        [HttpGet("{id}")]
+        public JsonResult GetOne(int id)
+        {
+            string query = @"
+                        SELECT * FROM servicio                        
+                        WHERE id=@ServicioId;
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@ServicioId", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult(table);
+
+        }
+
+        [HttpPost]
+        public JsonResult Post(Models.Servicio servicioData)
+        {
+            string query = @"
+                        INSERT INTO servicio (nombre, descripcion, imagen)
+                        VALUES (@Enombre, @Edescripcion, @Eimagen);         
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@Enombre", servicioData.nombre);
+                    myCommand.Parameters.AddWithValue("@Edescripcion", servicioData.descripcion);
+                    myCommand.Parameters.AddWithValue("@Eimagen", servicioData.imagen);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                        DELETE FROM servicio 
+                        WHERE id=@ServicioId;
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@ServicioId", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close(); 
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Models.Plato platoData)
+        {
+            string query = @"
+                        UPDATE servicio SET 
+                        nombre =@Enombre,
+                        descripcion =@Edescripcion,
+                        imagen =@Eimagen
+                        WHERE id =@PlatoId;   
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@PlatoId", platoData.id);
+                    myCommand.Parameters.AddWithValue("@Enombre", platoData.nombre);
+                    myCommand.Parameters.AddWithValue("@Edescripcion", platoData.descripcion);
+                    myCommand.Parameters.AddWithValue("@Eimagen", platoData.imagen);
+                    
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
     }
 }
